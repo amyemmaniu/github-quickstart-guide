b877d170964ee817e42c349c0f2e5fff6fb36864
amyemmaniu <amyemmaniu@gmail.com>
2025-09-03 21:22:33 +0800

    Updated dependencies

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -61,7 +61,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'Amy Niu - GitHub Quickstart Guide',
+        title: 'GitHub Quickstart Guide',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
